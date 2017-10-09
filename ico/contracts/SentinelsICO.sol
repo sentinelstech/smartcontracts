5f687bed4dd4956f5dbc875ada934364e81b649a
agurk <timothy.moll+github@gmail.com>
2017-10-09 22:56:51 +0200

    Update the sponsor recording to work properly. Lots of cleanups

--- ico/contracts/SentinelsICO.sol
+++ ico/contracts/SentinelsICO.sol
@@ -7,35 +7,44 @@ contract SentinelsICO {
     uint public ICOStart;
     uint public ICOClose;
 
+    mapping(address => uint) public sponsorAmounts;
+    mapping (address => address) sponsorAddrIdx;
+    address[] public sponsors;
+
     function SentinelsICO() {
         MinSatoshis = 1000;
         ICOStart = now;
         ICOClose = now + 14 days;
     }
 
-    function addSponsor(string name, uint amount) returns(bool) {
-        if (checkConditions(amount)) {
-            recordSponsor(name, amount);
-            return true;
-        }
-        return false;
+    function sponsor() payable returns (bool) {
+        require ( now < ICOClose );
+        require ( now > ICOStart );
+        require ( msg.value > MinSatoshis );
+
+        recordSponsor(msg.sender, msg.value);
+
+        return true;
     }
 
-    function checkConditions(uint amount) returns(bool) {
-        bool meetsConditions = true;
-
-        if ((now > ICOClose) || (now < ICOStart)) {
-            meetsConditions = false;
+    function getSponsors() public returns (address) {
+        address currentAddr = sponsorAddrIdx[0x0];
+        while (currentAddr != 0) {
+            console.log(currentAddr + " -- " + sponsorAmounts[currentAddr]);
+            currentAddr = sponsorAddrIdx[currentAddr];
         }
-
-        if (amount < MinSatoshis) {
-            meetsConditions = false;
-        }
-
-        return meetsConditions;
     }
 
-    function recordSponsor(string name, uint amount) {
-        // record in ledger
+    function addSponsorAddr(address _addr) public {
+        sponsorAddrIdx[_addr] = sponsorAddrIdx[0x0];
+        sponsorAddrIdx[0x0] = _addr;
+    }
+
+    function recordSponsor(address _addr, uint amount) {
+        if (sponsorAmounts[_addr] == 0) {
+            addSponsorAddr(_addr);
+        }
+        sponsorAmounts[_addr] += amount;
     }
 }
+
