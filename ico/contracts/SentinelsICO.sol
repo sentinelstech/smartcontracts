60dab2503a497c7f745da485a236b78d1a2fcc85
agurk <timothy.moll+github@gmail.com>
2017-10-18 19:57:04 +0200

    Adds ability to terminate contract at any time

--- ico/contracts/SentinelsICO.sol
+++ ico/contracts/SentinelsICO.sol
@@ -2,6 +2,10 @@ pragma solidity ^0.4.4;
 
 contract SentinelsICO {
 
+    address public owner;
+
+    bool contractLive;
+
     uint MinSatoshis;
 
     uint public ICOStart;
@@ -15,12 +19,15 @@ contract SentinelsICO {
         MinSatoshis = 1000;
         ICOStart = now;
         ICOClose = now + 14 days;
+        contractLive = true;
+        owner = msg.sender;
     }
 
     function sponsor() payable returns (bool) {
         require ( now < ICOClose );
         require ( now > ICOStart );
         require ( msg.value > MinSatoshis );
+        require (contractLive);
 
         recordSponsor(msg.sender, msg.value);
 
@@ -35,7 +42,7 @@ contract SentinelsICO {
         }
     }
 
-    function addSponsorAddr(address _addr) public {
+    function addSponsorAddr(address _addr) {
         sponsorAddrIdx[_addr] = sponsorAddrIdx[0x0];
         sponsorAddrIdx[0x0] = _addr;
     }
@@ -46,5 +53,19 @@ contract SentinelsICO {
         }
         sponsorAmounts[_addr] += amount;
     }
+
+    function closeICO() onlyBy (owner) {
+        contractLive = false;
+    }
+
+    function changeOwner(address _newOwner) onlyBy(owner) {
+        owner = _newOwner;
+    }
+
+    modifier onlyBy(address _account) {
+        require(msg.sender == _account);
+        _;
+    }
+
 }
 
